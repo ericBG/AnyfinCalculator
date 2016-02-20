ed19c5b1875d6e934212f9641c7bb69e1b3f235d
ericBG <ericrboidi@gmail.com>
2016-02-20 22:56:54 +0000

    Implement a simple graveyard helper class.

---
+++ AnyfinCalculator/AnyfinCalculator/GraveyardHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Hearthstone_Deck_Tracker.Hearthstone;
+using Hearthstone_Deck_Tracker.API;
+
+namespace AnyfinCalculator
+{
+    public class GraveyardHelper
+    {
+        private readonly Predicate<Card> _shouldBeTracked;
+        private List<Card> _checkedMinions = new List<Card>();
+
+        public GraveyardHelper(Predicate<Card> shouldBeTracked = null)
+        {
+            _shouldBeTracked = shouldBeTracked;
+        }
+
+        public IEnumerable<Card> TrackedMinions => Core.Game.Player.Graveyard.Select(ce => ce.Entity.Card)
+            .Concat(Core.Game.Opponent.Graveyard.Select(ce => ce.Entity.Card))
+            .Where(thisShouldBeAPredicate =>_shouldBeTracked(thisShouldBeAPredicate));
+    }
+}
