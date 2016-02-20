12494c0d02f33522239f4eccd64246ce3693726e
ericBG <ericrboidi@gmail.com>
2016-02-20 22:23:53 +0000

    Added a helper class to be able to detect murlocs.

---
+++ AnyfinCalculator/AnyfinCalculator/Murlocs.cs
@@ -0,0 +1,25 @@
+﻿
+using Hearthstone_Deck_Tracker.Hearthstone;
+
+namespace AnyfinCalculator
+{
+    //MRGLGLG
+    public static class Murlocs
+    {
+        public static Card BluegillWarrior { get; }
+        public static Card GrimscaleOracle { get; }
+        public static Card MurlocWarleader { get; }
+        public static Card OldMurkEye { get; }
+        static Murlocs()
+        {
+            BluegillWarrior = Database.GetCardFromId("CS2_173");
+            GrimscaleOracle = Database.GetCardFromId("EX1_508");
+            MurlocWarleader = Database.GetCardFromId("EX1_507");
+            OldMurkEye = Database.GetCardFromId("EX1_062");
+        }
+        public static bool IsMurloc(Card card)
+        {
+            return card.Race == "Murloc";
+        }
+    }
+}
