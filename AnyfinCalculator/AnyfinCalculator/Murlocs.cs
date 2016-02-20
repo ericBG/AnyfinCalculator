df386cdc56772e37e96b57e057b88c35b600da80
ericBG <ericrboidi@gmail.com>
2016-02-20 23:06:57 +0000

    Added "Anyfin Can Happen" to the Murlocs class.

--- AnyfinCalculator/AnyfinCalculator/Murlocs.cs
+++ AnyfinCalculator/AnyfinCalculator/Murlocs.cs
@@ -10,12 +10,14 @@ namespace AnyfinCalculator
         public static Card GrimscaleOracle { get; }
         public static Card MurlocWarleader { get; }
         public static Card OldMurkEye { get; }
+        public static Card AnyfinCanHappen { get; }
         static Murlocs()
         {
             BluegillWarrior = Database.GetCardFromId("CS2_173");
             GrimscaleOracle = Database.GetCardFromId("EX1_508");
             MurlocWarleader = Database.GetCardFromId("EX1_507");
             OldMurkEye = Database.GetCardFromId("EX1_062");
+            AnyfinCanHappen = Database.GetCardFromId("LOE_026");
         }
         public static bool IsMurloc(Card card)
         {
