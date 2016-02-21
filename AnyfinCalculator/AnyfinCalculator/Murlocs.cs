3437061a2a8cc045d84a5434175dfadc9b047245
ericBG <ericrboidi@gmail.com>
2016-02-21 00:33:09 +0000

    Yet more helper methods (big commit is about to happen)

--- AnyfinCalculator/AnyfinCalculator/Murlocs.cs
+++ AnyfinCalculator/AnyfinCalculator/Murlocs.cs
@@ -1,16 +1,10 @@
-﻿
-using Hearthstone_Deck_Tracker.Hearthstone;
+﻿using Hearthstone_Deck_Tracker.Hearthstone;
 
 namespace AnyfinCalculator
 {
     //MRGLGLG
     public static class Murlocs
     {
-        public static Card BluegillWarrior { get; }
-        public static Card GrimscaleOracle { get; }
-        public static Card MurlocWarleader { get; }
-        public static Card OldMurkEye { get; }
-        public static Card AnyfinCanHappen { get; }
         static Murlocs()
         {
             BluegillWarrior = Database.GetCardFromId("CS2_173");
@@ -19,14 +13,17 @@ namespace AnyfinCalculator
             OldMurkEye = Database.GetCardFromId("EX1_062");
             AnyfinCanHappen = Database.GetCardFromId("LOE_026");
         }
-        public static bool IsMurloc(this Card card)
-        {
-            return card.Race == "Murloc";
-        }
 
-        public static bool IsChargeMurloc(this Card card)
-        {
-            return card.Id == OldMurkEye.Id || card.Id == BluegillWarrior.Id;
-        }
+        public static Card BluegillWarrior { get; }
+        public static Card GrimscaleOracle { get; }
+        public static Card MurlocWarleader { get; }
+        public static Card OldMurkEye { get; }
+        public static Card AnyfinCanHappen { get; }
+        public static bool IsMurloc(this Card card) => card.Race == "Murloc";
+        public static bool IsChargeMurloc(this Card card) => card.Id == OldMurkEye.Id || card.Id == BluegillWarrior.Id;
+        public static bool IsBluegill(this Card card) => card.Id == BluegillWarrior.Id;
+        public static bool IsGrimscale(this Card card) => card.Id == GrimscaleOracle.Id;
+        public static bool IsWarleader(this Card card) => card.Id == MurlocWarleader.Id;
+        public static bool IsOldMurkEye(this Card card) => card.Id == OldMurkEye.Id;
     }
 }
