bdcfcfdf939562b9cda951039fc50d1e72c4391c
ericBG <ericrboidi@gmail.com>
2016-04-02 07:48:56 +0100

    Yet again I forgot to update the version string.

--- AnyfinCalculator/AnyfinCalculator/AnyfinPlugin.cs
+++ AnyfinCalculator/AnyfinCalculator/AnyfinPlugin.cs
@@ -61,7 +61,7 @@ namespace AnyfinCalculator
 
         public string ButtonText => "Options";
         public string Author => "ericBG";
-        public Version Version => new Version(1, 0, 3);
+        public Version Version => new Version(1, 0, 4);
         public MenuItem MenuItem => null;
     }
 }
